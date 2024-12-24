8de83881ba48c342ba4722d52a70f4ce7c3eafe3
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-25 00:27:07 +0330

    fix - Add columns to Treatments relation filament

--- app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -38,6 +38,11 @@ class TreatmentsRelationManager extends RelationManager
             ->recordTitleAttribute('description')
             ->columns([
                 Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('USD')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
             ])
             ->filters([
                 //
